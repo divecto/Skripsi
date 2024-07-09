0152d246b3a6dfecb4c77f3be75dc5fe06584938
divecto <102302608+divecto@users.noreply.github.com>
2024-07-09 10:56:31 +0700

    Update ResNet.py

--- Src/backbone/ResNet.py
+++ Src/backbone/ResNet.py
@@ -10,38 +10,6 @@ def conv3x3(in_planes, out_planes, stride=1):
                      padding=1, bias=False)
 
 
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-
 class Bottleneck(nn.Module):
     expansion = 4
 
@@ -82,25 +50,22 @@ class Bottleneck(nn.Module):
 
 
 class ResNet_2Branch(nn.Module):
-    # ResNet50 with two branches (modified from torchvision.models.resnet (pytorch==0.4.1))
     def __init__(self):
-        # self.inplanes = 128
         self.inplanes = 64
         super(ResNet_2Branch, self).__init__()
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
-                               bias=False)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
         self.layer1 = self._make_layer(Bottleneck, 64, 3)
         self.layer2 = self._make_layer(Bottleneck, 128, 4, stride=2)
-        self.layer3_1 = self._make_layer(Bottleneck, 256, 6, stride=2)
+        self.layer3_1 = self._make_layer(Bottleneck, 256, 23, stride=2)
         self.layer4_1 = self._make_layer(Bottleneck, 512, 3, stride=2)
 
         self.inplanes = 512
-        self.layer3_2 = self._make_layer(Bottleneck, 256, 6, stride=2)
+        self.layer3_2 = self._make_layer(Bottleneck, 256, 23, stride=2)
         self.layer4_2 = self._make_layer(Bottleneck, 512, 3, stride=2)
 
         for m in self.modules():
@@ -115,15 +80,14 @@ class ResNet_2Branch(nn.Module):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
+                nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(planes * block.expansion),
             )
 
         layers = []
         layers.append(block(self.inplanes, planes, stride, downsample))
         self.inplanes = planes * block.expansion
-        for i in range(1, blocks):
+        for _ in range(1, blocks):
             layers.append(block(self.inplanes, planes))
 
         return nn.Sequential(*layers)
